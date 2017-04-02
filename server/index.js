eae31a9fcb1459f04cb24489f11e861ad79612dc
Shaivi Vashistha <shaivi.vashistha@gmail.com>
2017-04-02 17:37:45 +0530

    Changes for OpenShift

--- server/index.js
+++ server/index.js
@@ -55,9 +55,9 @@ app.route('/*').get(function(req, res){
   res.sendFile('index.html', {root: __dirname + '/../client'});
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.listen(server_port, server_ip_address, function () {
 
